90b135e713a59d1627ed9c51b8936616a80c8ea4
Vulcan <vulcan.lee@gmail.com>
2021-09-19 21:52:34 +0800

    迭代器 Iterator : IEnumerator與IEnumerable 介面實作方法

---
+++ Examples/cs001/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace cs001
+{
+    // 迭代器 Iterator IEnumerator與IEnumerable 介面實作方法
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyIntIterator = new MyIntIterator();
+            #region 使用 foreach 
+            foreach (var item in MyIntIterator)
+                Console.WriteLine(item);
+            #endregion
+
+            #region 使用 MoveNext 來瀏覽所有紀錄
+            MyIntIterator.Reset();
+            while (MyIntIterator.MoveNext())
+                Console.WriteLine(MyIntIterator.Current);
+            #endregion
+        }
+    }
+
+    /// <summary>
+    /// 實作出 迭代器
+    /// </summary>
+    public class MyIntIterator : IEnumerator, IEnumerable
+    {
+        int[] data = { 1, 2, 3 };
+        int currentIndex = -1;
+
+        #region IEnumerable 介面實作方法
+        public IEnumerator GetEnumerator() { return this; }
+        #endregion
+
+        #region IEnumerator 介面實作方法
+        public object Current { get { return data[currentIndex]; } }
+        public bool MoveNext()
+        {
+            if (currentIndex >= data.Length - 1)
+                return false;
+            return ++currentIndex < data.Length;
+        }
+        public void Reset() { currentIndex = -1; }
+        #endregion
+    }
+}
---
+++ Examples/cs003/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace cs003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyIntIterator = new MyIntIterator();
+            #region 使用 foreach 
+            foreach (var item in MyIntIterator)
+                Console.WriteLine(item);
+            foreach (var item in MyIntIterator)
+                Console.WriteLine(item);
+            #endregion
+
+            #region 使用 MoveNext 來瀏覽所有紀錄(這裡無法使用)
+            //MyIntIterator.Reset();
+            //while (MyIntIterator.MoveNext())
+            //    Console.WriteLine(MyIntIterator.Current);
+            #endregion
+        }
+    }
+
+    /// <summary>
+    /// 實作出 迭代器
+    /// </summary>
+    public class MyIntIterator : IEnumerable
+    {
+        int[] data = { 1, 2, 3 };
+
+        #region IEnumerable 介面實作方法
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in data)
+            {
+                yield return item;
+            }
+        }
+        #endregion
+
+    }
+}
