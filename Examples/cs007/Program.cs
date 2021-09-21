c2116bda95a3fb228c303153be427639f59a4e43
Vulcan <vulcan.lee@gmail.com>
2021-09-21 19:56:24 +0800

    cs007|反覆運算器的應用範例

---
+++ Examples/cs007/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
