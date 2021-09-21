8bb9bcf1cf2a8644e19395f9793231775d584cbb
Vulcan <vulcan.lee@gmail.com>
2021-09-21 15:40:50 +0800

    cs006|列舉器 與  foreach 陳述式的運作方式

---
+++ Examples/cs006/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace cs006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello World!";
+
+            #region 使用 Foreach 陳述式來瀏覽
+            foreach (var item in myString)
+            {
+                Console.Write(item);
+            }
+            #endregion
+            Console.WriteLine();
+
+            #region 使用列舉器來瀏覽
+            CharEnumerator charEnumerator = myString.GetEnumerator();
+            while(charEnumerator.MoveNext())
+            {
+                Console.Write(charEnumerator.Current);
+            }
+            #endregion
+        }
+    }
+}
